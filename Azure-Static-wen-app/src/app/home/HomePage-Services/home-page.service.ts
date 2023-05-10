1a52df4a64fe61215402aaeb85f82945b11de575
divya1152002 <80250802+divya1152002@users.noreply.github.com>
2023-05-10 22:50:41 +0530

    Add files via upload

---
+++ Azure-Static-wen-app/src/app/home/HomePage-Services/home-page.service.ts
@@ -0,0 +1,92 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError, Observable, throwError } from 'rxjs';
+import { IProduct } from '../Home-Interfaces/IProduct';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HomePageService {
+
+
+
+ 
+
+  products: IProduct[]=[];
+  constructor(private http: HttpClient) 
+  {
+   
+  }
+
+  //Getting the Products from backend API
+  getProducts():Observable<IProduct[]>{
+    let tempVar = this.http.get<IProduct[]>('https://quickkartbackend.azurewebsites.net/api/home/getproducts')
+    console.log(tempVar)
+    return tempVar
+  }
+
+  MakePayment(CardNumber1:string,cvv1:string,ex:string,pid:number,cost:number):Observable<boolean>{
+
+    var pay:Payment
+    pay={cardNumber:CardNumber1,CVV:cvv1,Expiry:ex,ProdCost:cost,ProdID:pid}
+    console.log(pay)
+
+    let tempVar = this.http.post<boolean>(' http://localhost:7142/api/Function1',pay)
+    return tempVar
+  }
+
+  PostNewSubscriber(emailID:string):Observable<boolean>{
+  
+    console.log(emailID)
+
+    let tempVar = this.http.get<boolean>('https://localhost:5001/api/customer/AddNewSubscriber?emailID='+emailID)
+    console.log(tempVar)
+    return tempVar
+  }
+
+  
+  ValidateUser(userEmailID:string, userPassword:string, type:string):Observable<number>
+  {
+    var user:User
+    user={emailID:userEmailID, password:userPassword,usertype:type};
+    console.log(user)
+
+    let result=this.http.post<number>('https://quickkart-login.azurewebsites.net/api/LoginFunction?code=ymHedqB74Tir5u11AnNm6kEzoinr8COSWiXJ0ElpiAl-AzFuqRvUjg==',user)
+    return result
+
+  }
+
+  public uploadImage(image: File): Observable<Response>{
+    const formData = new FormData();
+   
+    formData.append('image', image);
+    console.log(formData)
+    let result=this.http.post<Response>('https://quickkart-backend.azurewebsites.net/api/admin/upload',formData).pipe(catchError(this.errorHandler))
+    console.log(result)
+    return result
+  }
+
+  errorHandler(error: HttpErrorResponse) {
+    console.log(error);
+    return throwError(error.message|| "server error")
+  }
+}
+
+export class User{
+
+  emailID:string='';
+  password:string='';
+  usertype:string='';
+
+
+}
+
+export class Payment{
+
+  cardNumber:string='';
+  CVV:string='';
+  Expiry:string='';
+  ProdCost:number=0;
+  ProdID:number=0;
+
+}
---
+++ Azure-Static-wen-app/src/app/home/home.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LandingPageComponent } from 'src/app/home/landing-page/landing-page.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatInputModule} from '@angular/material/input'
+import {MatIconModule} from '@angular/material/icon';
+import { HttpClientModule } from '@angular/common/http';
+import { NotifierModule } from 'angular-notifier';
+import {MatDialogModule} from '@angular/material/dialog';
+import { DialogComponent } from './dialog/dialog.component';
+import { LoginComponent } from './login/login.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatRadioModule } from '@angular/material/radio';
+import { CommonLayoutComponent } from './common-layout/common-layout.component';
+import { UserLayoutComponent } from './user-layout/user-layout.component';
+import { AdminViewComponent } from './admin-view/admin-view.component';
+import { ToolbarModule } from '@syncfusion/ej2-angular-navigations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatDialog } from '@angular/material/dialog';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import { PaymentComponent } from './payment/payment.component';
+@NgModule({
+  declarations: [
+    LandingPageComponent, DialogComponent, LoginComponent, CommonLayoutComponent, UserLayoutComponent, AdminViewComponent, PaymentComponent
+
+
+  ],
+  imports: [FormsModule,
+    ToolbarModule,MatListModule,MatButtonModule,MatSidenavModule,MatToolbarModule,CommonModule,MatRadioModule,NgbModule,MatFormFieldModule,MatDialogModule,MatCardModule,MatButtonModule,FormsModule,ReactiveFormsModule,MatInputModule,MatIconModule,HttpClientModule,NotifierModule],
+  exports:[
+
+    LandingPageComponent
+  ]
+})
+export class HomeModule { }
